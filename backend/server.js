79cc21228565d6f14da92677e0ea42597da4910e
Aizistral <admin@aizistral.com>
2024-05-12 23:12:10 +0200

    Implement link creation functionality

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,45 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/api/create-link", (req, res) => {
+  const { link } = req.body;
+
+  if (!link) return res.status(400).send("Link is required");
+  
+  const data = fs.readFileSync("db.json");
+  const links = JSON.parse(data);
+
+  const existingLink = links.find((l) => l.url === link);
+  if (existingLink) return res.json(existingLink);
+
+  // Collision is possible but addressing this is out of the scope of this project
+  const linkID = Math.random().toString(36).substring(2, 9);
+  const newLink = { id: linkID, url: link };
+
+  links.push(newLink);
+
+  fs.writeFileSync("db.json", JSON.stringify(links));
+
+  return res.json(newLink);
+});
+
+app.get("/:linkID", (req, res) => {
+  const { linkID } = req.params;
+
+  const data = fs.readFileSync("db.json");
+  const links = JSON.parse(data);
+
+  const link = links.find((link) => link.id === linkID);
+
+  if (!link) return res.status(404).send("Link not found");
+
+  return res.redirect(link.url);
 });
 
 app.listen(port, () => {
